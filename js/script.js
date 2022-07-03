2f1f920c39eb246a9fd672280a7c4125b6b787e3
vitormalencar <vitor.maia.alencar@gmail.com>
2022-07-03 16:53:44 -0300

    cleaning up =P

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
-// fetch cards from API and display them in the DOM localhost 3001
-function renderCard({ title, description, image }) {
+const URL =
+  "https://my-json-server.typicode.com/vitormalencar/flash-app/definitions";
+
+function fetchCards() {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => renderCard(data));
+}
+
+function renderCard(cards) {
+  const index = Math.floor(Math.random() * cards.length);
+  const card = cards[index];
+  const cardContainer = document.querySelector("#card-container");
+
+  cardContainer.innerHTML = "";
+
   const cardDiv = document.createElement("div");
+
   cardDiv.classList.add("card", "animate__animated", "animate__backInRight");
+
   cardDiv.innerHTML = `
-    <div class="card-image">
-    </div>
-    <div class="card-wrapper">
-      <div class="card-title">${title}</div>
-      <p class="card-description">${description}</p>
+    <div class="card-image"></div>
+    <div class="card-content">
+      <div class="card-title">${card.title}</div>
+      <p class="card-description">${card.description}</p>
     </div>
   `;
-  return cardDiv;
-}
-function getCards() {
-  fetch(
-    "https://my-json-server.typicode.com/vitormalencar/flash-app/definitions"
-  )
-    .then((response) => response.json())
-    .then((cards) => {
-      const index = Math.floor(Math.random() * cards.length);
-      const card = cards[index];
-      const cardContainer = document.querySelector(".cards");
-      const cardDiv = renderCard(card);
 
-      cardContainer.innerHTML = "";
-      cardContainer.appendChild(cardDiv);
-    });
+  cardContainer.appendChild(cardDiv);
 }
 
-window.addEventListener("load", getCards);
+window.addEventListener("load", fetchCards);
